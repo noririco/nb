dfb8bb4a82493401c275bc12171ca51a42bc5b09
noririco <nisanlevy666@gmail.com>
2021-08-09 17:19:08 +0300

    remove log to file

--- logger.js
+++ logger.js
@@ -11,15 +11,15 @@ module.exports = createLogger({
         )
       ),
     }),
-    new transports.File({
-      filename: "server.log",
-      format: format.combine(
-        format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
-        format.align(),
-        format.printf(
-          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
-        )
-      ),
-    }),
+    // new transports.File({
+    //   filename: "server.log",
+    //   format: format.combine(
+    //     format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+    //     format.align(),
+    //     format.printf(
+    //       (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+    //     )
+    //   ),
+    // }),
   ],
 });
